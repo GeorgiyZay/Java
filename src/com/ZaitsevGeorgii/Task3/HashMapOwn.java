b219ca13f32c8fc27edb79044c21e65a55b5bd24
GeorgiyZay <goshazay@mail.ru>
2020-12-07 04:54:08 +0300

    Task3 without test

---
+++ src/com/ZaitsevGeorgii/Task3/HashMapOwn.java
@@ -0,0 +1,70 @@
+package com.ZaitsevGeorgii.Task3;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapOwn implements java.util.Map {
+
+    private HashTableOwn<Object, Object> table;
+
+    @Override
+    public Object get(Object key) {
+        return table.get(key);
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return table.add(key, value);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return table.getSize() == 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+
+    @Override
+    public Object remove(Object key) {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+
+    @Override
+    public void putAll(Map m) {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+
+    @Override
+    public Set keySet() {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+
+    @Override
+    public Collection values() {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        throw new UnsupportedOperationException("Не реализован");
+    }
+}
---
+++ src/com/ZaitsevGeorgii/Task3/HashTableOwn.java
@@ -0,0 +1,105 @@
+package com.ZaitsevGeorgii.Task3;
+
+import java.util.ArrayList;
+
+public class HashTableOwn<K, V> {
+
+    private static class NodeOfHash<K, V> {
+        NodeOfHash<K, V> next;
+        K key;
+        V value;
+
+        public NodeOfHash(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private ArrayList<NodeOfHash<K, V>> table;
+    private int size;
+
+    public int getSize(){
+        return size;
+    }
+
+    public HashTableOwn(int size) {
+        table = new ArrayList<NodeOfHash<K, V>>();
+        size = 0;
+        for (int i = 0; i < size; i++) {
+            table.add(null);
+        }
+    }
+
+    private int getIndex(K key) {
+        return Math.abs(key.hashCode() % table.size());
+    }
+
+    public V get(K key) {
+
+        if (contains(key)) {
+            NodeOfHash<K, V> node = table.get(getIndex(key));
+
+            while (node != null) {
+                if (node.key.equals(key)) {
+                    return node.value;
+                }
+                node = node.next;
+            }
+        }
+        return null;
+    }
+
+    public boolean contains(K key) {
+        NodeOfHash<K, V> node = table.get(getIndex(key));
+
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return true;
+            }
+            node = node.next;
+        }
+        return true;
+    }
+
+    public V remove(K key) {
+        NodeOfHash<K, V> node = table.get(getIndex(key));
+        NodeOfHash<K, V> prevNode = null;
+        while (node != null) {
+            if (node.key.equals(key)) {
+                break;
+            }
+            prevNode = node;
+            node = node.next;
+        }
+        if (node == null) {
+            throw new NullPointerException("Не существует элемента с таким ключом");
+        } else if (prevNode != null) {
+            prevNode.next = node.next;
+        } else {
+            table.set(getIndex(key), node.next);
+        }
+        size--;
+        return node.value;
+    }
+
+    public V add(K key, V value){
+        int ind = getIndex(key);
+        NodeOfHash<K, V> node = table.get(ind);
+
+        while (node != null) {
+            if (node.key.equals(key)) {
+                V prevValue = node.value;
+                node.value = value;
+                return  prevValue;
+            }
+            node = node.next;
+        }
+        node = new NodeOfHash<K, V>(key, value);
+        if (table.get(ind) != null){
+            node.next = table.get(ind);
+        }
+        size++;
+        table.set(ind, node);
+        return null;
+    }
+}
